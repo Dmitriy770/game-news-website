bf9e84629a5cc806556a36b5cb1225ff36da98da
Dmitriy770 <zdima08@gmail.com>
2023-10-03 23:34:15 +0300

    work with oauth

--- frontend/game-news-frontend/src/routes/+layout.js
+++ frontend/game-news-frontend/src/routes/+layout.js
@@ -1,4 +1,4 @@
-import { goto } from "$app/navigation";
+import { goto } from '$app/navigation';
 
 export const load = async() => {
     const login = async () => {
@@ -7,8 +7,9 @@ export const load = async() => {
         
         if(code){
             urlSeatchParams.delete('code');
-            console.log(location.pathname + urlSeatchParams.toString());
-            await goto("/test")
+            var url = location.pathname + urlSeatchParams.toString();
+            console.log(url)
+            await goto(`${url}`, {replaceState: true})
 
             const res = await fetch(`http://localhost:8080/api/v1/oauth2/token?code=${code}`);
             const data = await res.json();
